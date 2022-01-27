fc9d54384c4b7ecb0d1863a87539821d2d666969
Michael Ramos <ramosmichael@live.com>
2022-01-27 01:09:20 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Mike",
+    streetAddress: "36 Fort Evans"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {...employee}
+
+    newEmployee[name];
+
+    delete newEmployee.name;
+
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employees, name) {
+    delete employee.name;
+
+    return employee;
+}
